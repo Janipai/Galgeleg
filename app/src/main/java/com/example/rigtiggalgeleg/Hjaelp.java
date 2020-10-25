7de1576824612d784832cf1063523c886c41930f
ShaniaHau <shaniahau@gmail.com>
2020-10-25 20:01:56 +0100

    Laver fragmenter senere i Hjaelp

--- app/src/main/java/com/example/rigtiggalgeleg/Hjaelp.java
+++ app/src/main/java/com/example/rigtiggalgeleg/Hjaelp.java
@@ -2,10 +2,12 @@ package com.example.rigtiggalgeleg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class Hjaelp extends AppCompatActivity {
+public class Hjaelp extends AppCompatActivity implements View.OnClickListener {
 
     Button regler, support;
 
@@ -16,5 +18,19 @@ public class Hjaelp extends AppCompatActivity {
 
         regler = (Button) findViewById(R.id.regler);
         regler.setOnClickListener(this);
+
+        support = (Button) findViewById(R.id.support);
+        support.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.regler){
+            //fragment
+        }
+        else if (v.getId()==R.id.regler){
+            //fragment
+        }
+
     }
 }
